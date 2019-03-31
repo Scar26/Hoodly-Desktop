1e4a9da47a47234ab40ad65278067b8b42e4a3d7
Scar26 <mmatty26@gmail.com>
2019-04-01 03:59:23 +0530

    ejs integration started

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const electron=require('electron');
 const url = require('url');
 const path = require('path');
+const express = require('express');
+const server = express();
+const exserver = require('http').createServer(server);
+const session = require('express-session');
+const parser = require('body-parser');
+const io = require('socket.io').listen(exserver);
 
-const {app, BrowserWindow, Menu} = electron;
+process.env.NODE_ENV = 'development'; //change to production for release builds
+
+server.use(parser.json());
+server.use(parser.urlencoded({ extended: true}));
+
+server.use(session({
+	name : 'sID',
+	secret : '#14@4%=%^25$',
+	resave: false,
+	saveUninitialized:false
+
+}));
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 let mainWindow;
 app.on('ready',function(){
 	mainWindow = new BrowserWindow({});
@@ -14,6 +33,9 @@ app.on('ready',function(){
 	}));
 	const mainmenu = Menu.buildFromTemplate(menutemp);
 	Menu.setApplicationMenu(mainmenu);
+	if(process.env.NODE_ENV!='development'){
+		mainWindow.setMenuBarVisibility(false);
+	}
 });
 
 
@@ -30,7 +52,7 @@ function profile(){
 	}));
 }
 
-
+// template for the top-menu
 const menutemp = [{
 	label:'user',
 	submenu:[{
@@ -47,3 +69,57 @@ const menutemp = [{
 		click(){app.quit();}
 	}]
 }];
+
+//for ease in development
+if(process.env.NODE_ENV=='development'){
+	menutemp.push({
+		label : 'Developer tools',
+		submenu :[{
+			label: "Dev Tools",
+			click(item,focusedWindow){focusedWindow.toggleDevTools();},
+			accelerator: 'ctrl + i'
+		},{
+			role : 'reload'			
+		}]
+	})
+}
+
+//express starts here
+
+exserver.listen(process.env.PORT || 3000); //Set up a server at port 3000 for dynamic requests
+
+//server.use('view engine','ejs');
+
+
+server.get('/login',function(req,res){
+	$auth='SELECT * FROM `user` where `name` ="'+req.body.uname+'" and `pass` ="'+req.body.pass+'"';
+	connection.query($auth,(err,rows,fields)=>{
+	console.log(rows);
+	if(rows[0]){
+			req.session.uname = req.body.uname;
+			console.log(req.body.uname+" added to the session");
+			res.sendFile(__dirname+'/chat.html');
+
+			}
+	else{
+			res.sendFile(__dirname+'/Invalid credentials');
+			}
+		}); 
+});
+
+//handle requests to open a community
+
+server.get('/communities/:com/:page',function(req,res){
+	console.log(req.params);
+	if(req.params.page=='dashboard'){
+		res.render('comdash.ejs',{comname:req.params.com}); //for non-members
+	}
+	else{
+		res.render('commain',{ comname:req.params.com}); // for members of the community
+	}
+});
+
+//image routing
+server.get('/images/dispics/:com',function(req,res){
+	res.sendFile(__dirname+'/coms/dispics/'+req.params.com+'.jpg');
+});
