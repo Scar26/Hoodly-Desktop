ec3553dbc40e42fb3340b29294e533f044cd5d47
Scar26 <mmatty26@gmail.com>
2019-04-15 20:03:17 +0530

    bundle.py addded

---
+++ bundle.py
@@ -0,0 +1,30 @@
+import mysql.connector as SQL
+
+DB = SQL.connect(
+	host = "localhost",
+	user = "root",
+	passwd = ""
+	)
+
+ctx = DB.cursor()
+ctx.execute("SHOW DATABASES")
+
+if not ('city' in ctx.fetchall()):
+	ctx.execute("CREATE DATABASE")
+
+city = SQL.connect(
+		host = "localhost",
+		user = "root",
+		passwd = "",
+		database = "city"
+		)
+
+ctx.execute("CREATE TABLE communities (name VARCHAR(255), college VARCHAR(255)")
+
+query = "INSERT INTO communities (name,college) VALUES (%s,%s)"
+entries = [("roorkee_anime","roorkee"),("roorkee_developers","roorkee"),("roorkee_literature","roorkee"),("roorkee_marvelgeeks","roorkee")]
+for val in entries:
+	ctx.execute(query,val)
+	city.commit
+
+print("database created")
